6746d2749859161e2accd0311fd7346ac181d254
Golapadeog <jeremy.landon@outlook.com>
2021-04-23 23:28:57 +0200

    fix after dio 4.0 breaking changes

--- dio_retry/lib/src/retry_interceptor.dart
+++ dio_retry/lib/src/retry_interceptor.dart
@@ -1,50 +1,66 @@
 import 'package:dio/dio.dart';
 import 'package:logging/logging.dart';
-import 'package:meta/meta.dart';
 
 import 'options.dart';
 
 /// An interceptor that will try to send failed request again
 class RetryInterceptor extends Interceptor {
   final Dio dio;
-  final Logger logger;
+  final Logger? logger;
   final RetryOptions options;
 
-  RetryInterceptor({@required this.dio, this.logger, RetryOptions options})
-      : this.options = options ?? const RetryOptions();
+  RetryInterceptor({required this.dio, this.logger, RetryOptions? options}) : options = options ?? const RetryOptions();
 
   @override
-  Future  onError(DioError err, ErrorInterceptorHandler handler) async {
-    var extra = RetryOptions.fromExtra(err.requestOptions) ?? this.options;
+  Future onError(DioError err, ErrorInterceptorHandler handler) async {
+    var extra = RetryOptions.fromExtra(err.requestOptions) ?? options;
 
-    var shouldRetry = extra.retries > 0 && await extra.retryEvaluator(err);
-    if (shouldRetry) {
-      if (extra.retryInterval.inMilliseconds > 0) {
-        await Future.delayed(extra.retryInterval);
-      }
-
-      // Update options to decrease retry count before new try
-      extra = extra.copyWith(retries: extra.retries - 1);
-      err.requestOptions.extra = err.requestOptions.extra..addAll(extra.toExtra());
-
-      try {
-        logger?.warning(
-            "[${err.requestOptions.uri}] An error occured during request, trying a again (remaining tries: ${extra.retries}, error: ${err.error})");
-        // We retry with the updated options
-       return await this.dio.request(
-          err.requestOptions.path,
-          cancelToken: err.requestOptions.cancelToken,
-          data: err.requestOptions.data,
-          onReceiveProgress: err.requestOptions.onReceiveProgress,
-          onSendProgress: err.requestOptions.onSendProgress,
-          queryParameters: err.requestOptions.queryParameters,
-          options: extra.toOptions()
-        );
-      } catch (e) {
-        return e;
-      }
+    final shouldRetry = extra.retries > 0 && await options.retryEvaluator(err);
+    if (!shouldRetry) {
+      return super.onError(err, handler);
     }
 
-    return super.onError(err, handler);
+    if (extra.retryInterval.inMilliseconds > 0) {
+      await Future.delayed(extra.retryInterval);
+    }
+
+    // Update options to decrease retry count before new try
+    extra = extra.copyWith(retries: extra.retries - 1);
+    err.requestOptions.extra = err.requestOptions.extra..addAll(extra.toExtra());
+
+    try {
+      logger?.warning('[${err.requestOptions.uri}] An error occurred during request, trying a again (remaining tries: ${extra.retries}, error: ${err.error})');
+      // We retry with the updated options
+      return await dio.request(
+        err.requestOptions.path,
+        cancelToken: err.requestOptions.cancelToken,
+        data: err.requestOptions.data,
+        onReceiveProgress: err.requestOptions.onReceiveProgress,
+        onSendProgress: err.requestOptions.onSendProgress,
+        queryParameters: err.requestOptions.queryParameters,
+        options: toOptions(err.requestOptions),
+      );
+    } catch (e) {
+      return e;
+    }
+  }
+
+  Options toOptions(RequestOptions o) {
+    return Options(
+      method: o.method,
+      sendTimeout: o.sendTimeout,
+      receiveTimeout: o.receiveTimeout,
+      extra: o.extra,
+      headers: o.headers,
+      responseType: o.responseType,
+      contentType: o.contentType,
+      validateStatus: o.validateStatus,
+      receiveDataWhenStatusError: o.receiveDataWhenStatusError,
+      followRedirects: o.followRedirects,
+      maxRedirects: o.maxRedirects,
+      requestEncoder: o.requestEncoder,
+      responseDecoder: o.responseDecoder,
+      listFormat: o.listFormat,
+    );
   }
 }
